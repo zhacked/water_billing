ae5b05a77ee24962ff20b9c6cb34d11f3963acdc
zhacked <fsiapco@gmail.com>
2025-06-19 13:26:12 +0800

    fix: customer

---
+++ resources/views/pages/customer/form.blade.php
@@ -0,0 +1,90 @@
+@extends('layouts.master')
+
+@section('subtitle', isset($customer) ? 'Edit customer' : 'Add New customer')
+@section('content_header_title', isset($customer) ? 'Update customer' : 'Create customer')
+
+@section('content_body')
+<div class="row">
+    <div class="col-md-12  mt-4">
+
+        {{-- Back Button --}}
+        <a href="{{ route('customer.index') }}" class="btn btn-secondary mb-3">
+            <i class="fas fa-arrow-left"></i> Back
+        </a>
+
+        <div class="card card-primary">
+            <div class="card-header">
+                <h3 class="card-title">{{ isset($customer) ? 'Edit' : 'New' }} customer Information</h3>
+            </div>
+
+            <form 
+                method="POST" 
+                action="{{ isset($customer) ? route('customer.update', $customer->id) : route('customer.store') }}"
+            >
+                @csrf
+                @if(isset($customer))
+                    @method('PUT')
+                @endif
+
+                <div class="card-body">
+
+                    <x-form.input 
+                        label="Full Name" 
+                        name="name" 
+                        type="text" 
+                        placeholder="Enter full name" 
+                        :value="old('name', $customer->name ?? '')"
+                    />
+
+                    <x-form.input 
+                        label="Email Address" 
+                        name="email" 
+                        type="email" 
+                        placeholder="Enter email" 
+                        :value="old('email', $customer->email ?? '')"
+                    />
+
+                    <x-form.input 
+                        label="Address" 
+                        name="address" 
+                        type="text" 
+                        placeholder="Enter address" 
+                        :value="old('address', $customer->address ?? '')"
+                    />
+
+                    <x-form.input 
+                        label="Contact Number" 
+                        name="contact_number" 
+                        type="text" 
+                        placeholder="Enter phone number"
+                        :value="old('contact_number', $customer->contact_number ?? '')"
+                    />
+
+                      <x-form.input 
+                        label="Meter Number" 
+                        name="meter_number" 
+                        type="text" 
+                        placeholder="Enter Meter Number"
+                        :value="old('meter_number', $customer->meter_number ?? '')"
+                    />
+
+                    <x-form.select 
+                        label="Group name :  {{ isset($customer)  ?  $customer?->group->name : 'No group chosen'}}" 
+                        name="group_id" 
+                        :options="$groups->pluck('name', 'id')->toArray()" 
+                        :selected="old('group_id', isset($group) ? $group->id : '')" 
+                    />
+
+                </div>
+
+                <div class="card-footer text-right">
+                    <x-form.submit-button 
+                        :label="isset($customer) ? 'Update' : 'Submit'" 
+                        class="btn btn-success" 
+                    />
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+@stop
